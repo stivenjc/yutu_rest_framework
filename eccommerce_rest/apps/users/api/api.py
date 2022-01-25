af5cd4cc47c15dfd606d87069aebbc3d56afae74
stiven <adisjimenez@misena.edu.co>
2022-01-25 15:49:54 -0500

    aprendiendo los serializer de manera muy basica

--- eccommerce_rest/apps/users/api/api.py
+++ eccommerce_rest/apps/users/api/api.py
@@ -1,19 +1,66 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializers
+from apps.users.api.serializers import UserSerializers, TestUserserializer
+
 
 @api_view(['GET', 'POST'])
 def user_api_view(request):
+    # List
     if request.method == 'GET':
         users = User.objects.all()
         user_serializer = UserSerializers(users, many=True)
-        return Response(user_serializer.data)
 
+
+        test_data = {
+            'name': 'adrian',
+            'email': 'jimenezcardenasd@gmail.com'
+        }
+        test_user = TestUserserializer(data=test_data)
+        if test_user.is_valid():
+            print('paso la validacion')
+
+
+        return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+    # create
     elif request.method == 'POST':
-        user_serializer = UserSerializers(data = request.data)
+        user_serializer = UserSerializers(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response(user_serializer.data)
-        return Response(user_serializer.errors)
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk=None):
+    # queryset
+    user = User.objects.filter(id=pk).first()
+
+    # validation
+    if user:
+
+        # retrieve
+        if request.method == 'GET':
+            user = User.objects.filter(id=pk).first()
+            user_serializer = UserSerializers(user)
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+        # update
+        elif request.method == 'PUT':
+            user = User.objects.filter(id=pk).first()
+            user_serializer = UserSerializers(user, data=request.data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.errors)
+
+        # delete
+        elif request.method == 'DELETE':
+            user = User.objects.filter(id=pk).first()
+            user.delete()
+            return Response({'message': 'usuário eliminado'})
+
+    return Response({'messge': 'no se ha encontrado un usurio con estos datos'}, status=status.HTTP_400_BAD_REQUEST)
--- eccommerce_rest/apps/users/api/serializers.py
+++ eccommerce_rest/apps/users/api/serializers.py
@@ -6,3 +6,8 @@ class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class TestUserserializer(serializers.Serializer):
+    name = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
