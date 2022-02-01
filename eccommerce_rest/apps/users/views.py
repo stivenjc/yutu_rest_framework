0fc9972f68079ef6cdf79851ff296c32461dad34
stiven <adisjimenez@misena.edu.co>
2022-02-01 09:43:27 -0500

    haciendo el logout ocn los tokens

--- eccommerce_rest/apps/users/views.py
+++ eccommerce_rest/apps/users/views.py
@@ -6,10 +6,12 @@ from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.users.api.serializers import UserTokenSerializer
 
 
+# craando el loggin
 class Login(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
@@ -48,3 +50,34 @@ class Login(ObtainAuthToken):
         else:
             return Response({'error': 'nombre del usuario o contraseña incorrecta'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'mensaje': 'hola desde response'}, status=status.HTTP_200_OK)
+
+
+# creando el logout
+class Logout(APIView):
+
+    def get(self, request, *arg, **kwargs):
+        try:
+            token = request.GET.get('token')
+            token = Token.objects.filter(key=token).first()
+            print(token)
+
+            if token:
+                user = token.user
+
+                all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
+                if all_sessions.exists():
+                    for session in all_sessions:
+                        session_data = session.get_decoded()
+                        if user.id == int(session_data.get('_auth_user_id')):
+                            session.delete()
+
+                token.delete()
+
+                session_message = 'sesion edel usuario eliminadas.'
+                token_message = 'token eliminado.'
+                return Response({'token_message': token_message, 'session_message': session_message},
+                                status=status.HTTP_200_OK)
+            return Response({'error': 'no se a encontrado un usuario con estascredenciales'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        except:
+            Response({'error': 'no se a encontrdo token en descripcion'}, status=status.HTTP_409_CONFLICT)
--- eccommerce_rest/eccommerce_rest/urls.py
+++ eccommerce_rest/eccommerce_rest/urls.py
@@ -20,7 +20,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from apps.users.views import Login
+from apps.users.views import Login, Logout
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,5 +42,6 @@ urlpatterns = [
     re_path(r'^redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('usuario/', include('apps.users.api.urls')),
     path('', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
     path('products/', include('apps.products.api.routers')),
 ]
