04685210ce0cf5b39474b1090f3993ec97bc7c73
stiven <adisjimenez@misena.edu.co>
2022-02-03 15:29:37 -0500

    edicion y eliminacion de frontend y backend

--- eccommerce_rest/apps/products/api/views/general_views.py
+++ eccommerce_rest/apps/products/api/views/general_views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from apps.base.api import GeneralListApiView
@@ -34,5 +34,37 @@ class IndicatorViewset(viewsets.ModelViewSet):
     serializer_class = IndicatorSerializer
 
 
-class CategoryProductViewset(viewsets.ModelViewSet):
+class CategoryProductViewset(viewsets.GenericViewSet):
     serializer_class = CategoryProductSerializer
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state=True)
+
+    def get_object(self):
+        return self.get_serializer().Meta.model.objects.filter(id=self.kwargs['pk'], state=True)
+
+    def list(self, request):
+        data = self.get_queryset()
+        data = self.serializer_class(data, many=True)
+        return Response(data.data)
+
+    def create(self, requests):
+        serializer = self.serializer_class(data=requests.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Categoria registrada correctamente'}, status=status.HTTP_201_CREATED)
+        return Response({'message': '', 'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        if self.get_object().exists():
+            serializer = self.serializer_class(instance=self.get_object().get(), data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        if self.get_object().exists():
+            self.get_object().get().delete()
+            return Response({'message': 'Categoria eliminada corectamente'}, status=status.HTTP_200_OK)
+        return Response({'message': '', 'error': 'categoria no encontrada'})
